90736b2c66f209f935de0ae520ddfe404092e4c9
Danyello <ulkiora.panda@gmail.com>
2021-06-19 19:34:46 +0600

    added middleware component

---
+++ !mssqlLearn/LessonMonitor/Middleware/LoggingExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public static class LoggingExtensions
+    {
+        public static IApplicationBuilder UseLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+}
---
+++ !mssqlLearn/LessonMonitor/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public LoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<LoggingMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            finally
+            {
+                _logger.LogInformation(
+                    "Request {method} {url} => {statusCode}",
+                    context.Request?.Method,
+                    context.Request?.Path.Value,
+                    context.Response?.StatusCode);
+
+                //using (FileStream fs = new FileStream("Loggs", FileMode.OpenOrCreate))
+                //{
+                //    byte[] arr = Encoding.UTF8.GetBytes($"Request " +
+                //        $"{context.Request?.Method} " +
+                //        $"{context.Request?.Path.Value} => " +
+                //        $"{context.Response?.StatusCode}");
+
+                //    fs.Write(arr);
+                //}
+
+                using (StreamWriter sw = new StreamWriter("Loggs.txt", true, Encoding.UTF8))
+                {
+                    sw.WriteLine($"Request " +
+                        $"{context.Request?.Method} " +
+                        $"{context.Request?.Path.Value} => " +
+                        $"{context.Response?.StatusCode}" +
+                        $" | Execute time => {DateTime.Now}");
+                }
+            }
+        }
+    }
+}
